62ec57df3d6a44b3b231f389adffc38226a6d6cd
Veerkhushal <93868228+Veerkhushal@users.noreply.github.com>
2021-12-11 12:43:02 +0530

    Add files via upload

---
+++ school-1.py
@@ -0,0 +1,26 @@
+
+
+import pandas as pd
+l1=[]
+columns=["Name"]
+while True:
+    subject=input("Enter subject name ")
+    if subject=="":
+        break
+    columns.append(subject)
+
+while True:
+    l2=[]
+    name=input("Enter name of student ")
+    if name=="":
+        break
+    l2.append(name)
+    
+    for column in range(1,len(columns)):
+        data=int(input("Enter marks of {} ".format(columns[column])))
+        l2.append(data)
+    l1.append(l2)    
+dataframe=pd.DataFrame(l1)
+dataframe.columns=columns 
+print(dataframe)
+dataframe.to_csv("marks.csv")
